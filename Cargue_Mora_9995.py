67ea1de959646a120eb89c45ca5914927567cf74
Alejo141 <66750054+Alejo141@users.noreply.github.com>
2025-02-06 12:00:47 -0500

    Add files via upload

---
+++ Cargue_Mora_9995.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+import io
+
+st.title("Actualización de VALOR_MORA en CSV")
+
+# Cargar archivos desde el usuario
+archivo1 = st.file_uploader("Cargar archivo con 'Saldo de Factura'", type=['csv'])
+archivo2 = st.file_uploader("Cargar archivo a actualizar", type=['csv'])
+
+if archivo1 and archivo2:
+    # Leer los archivos CSV
+    df1 = pd.read_csv(archivo1)
+    df2 = pd.read_csv(archivo2)
+    
+    # Crear un diccionario para acceder rápidamente a los valores de 'Saldo de Factura'
+    saldo_factura_dict = df1.set_index('NIU')['Saldo_Factura'].to_dict()
+    
+    # Asegurarse de que las columnas sean numéricas
+    df2['VALOR_MORA'] = pd.to_numeric(df2['VALOR_MORA'], errors='coerce').fillna(0)
+    
+    # Actualizar el campo 'VALOR_MORA' en df2 con los valores del diccionario
+    df2['VALOR_MORA'] = df2['NIU'].map(saldo_factura_dict).fillna(df2['VALOR_MORA'])
+    
+    # Manejar los valores vacíos o 'NA' en el campo ID_FACTURA
+    df2['ID_FACTURA'] = df2['ID_FACTURA'].replace('', 'NA')
+    df2['ID_FACTURA'] = df2['ID_FACTURA'].fillna('NA')
+    
+    # Guardar el resultado en un buffer
+    output = io.BytesIO()
+    df2.to_csv(output, index=False, encoding='utf-8')
+    output.seek(0)
+    
+    # Botón para descargar el archivo actualizado
+    st.download_button(label="Descargar archivo actualizado", data=output, file_name="IUF1_122024.csv", mime="text/csv")
+    
+    st.success("Actualización completada.")
